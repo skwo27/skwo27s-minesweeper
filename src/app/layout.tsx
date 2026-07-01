df612f7fd9985343b9aef553ab1fd9015c7e50fb
skwo27 <165741912+skwo27@users.noreply.github.com>
2026-07-01 11:33:36 +0900

    fix: set document language to Korean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <main>
           {children}
